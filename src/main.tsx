127187e79d9d05fb9811716851ad6b04bdfa89d9
CPatrimonial <raul.pedro.mv@gmail.com>
2025-06-03 23:15:59 -0300

    com os erros detalhados
    sera´q foi?

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -22,7 +22,11 @@ const renderApp = () => {
   
   const root = document.getElementById("root");
   if (root) {
-    createRoot(root).render(<App />);
+    createRoot(root).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
   }
 };
 
