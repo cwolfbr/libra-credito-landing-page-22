c0ac25d588a1af9723fb6cf07eebc9b1d6381579
cwolfbr <raul.pedro.mv@gmail.com>
2025-07-02 18:17:34 -0300

    Add new customer testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -18,6 +18,21 @@ const testimonials = [
     name: "Emanuele Cristina Presenti",
     age: "27 anos",
     text: "Meu primeiro contato com a Libra foi pelo Google, pesquisei mais detalhes sobre a empresa e entrei em contato. A primeira comunicação foi ótima e os consultores também! Fiz cotação com outras empresas, mas o valor que a Libra liberou foi maior, e as condições de pagamento e taxa de juros também foram melhores."
+  },
+  {
+    name: "Rodrigo da Silva Reis Frias",
+    age: "44 anos",
+    text: "A gente se viu num momento bem difícil pós pandemia, somos do comércio, temos um restaurante. Nesse momento eu entrei no Google e digitei “empréstimo com garantia de imóvel para negativado” o primeiro link que apareceu foi o de vocês. Depois que eu preenchi o cadastro que tinha automático, em uns 10 minutos uma pessoa da equipe me ligou, aí já entrei em processo de avaliação. Foi tudo muito rápido. Eu pensava em um valor, mas daí a analista, através dos dados que eu passei, fez um levantamento de todas as dívidas que a gente tinha e me considerou um valor maior ainda e para que eu quitasse as dívidas e ainda ficasse com um dinheiro para capital de giro. Para mim o atendimento foi perfeito, eu estava num momento difícil, eu e a minha esposa brincamos que foi coisa de Deus. Eu tenho muita facilidade para pegar os boletos. Todo mês a gente chama no WhatsApp e o atendimento é muito rápido pelo robozinho."
+  },
+  {
+    name: "Jorge Gaulke",
+    age: "49 anos",
+    text: "Busquei o crédito pensando na melhoria da empresa que tenho com a minha esposa. Foi para comprar alguns equipamentos, para poder atender melhor nossa capacidade. E aí eu acabei descobrindo a Libra por pesquisas, pesquisei na internet e após a simulação um representante entrou em contato comigo, após levantarmos todas as documentações, fui aprovado. A Libra foi a melhor empresa que encontrei em relação a atendimento e facilidade no contrato."
+  },
+  {
+    name: "Valdirene Ruiz Garcia",
+    age: "42 anos",
+    text: "Procurando financiar meu projeto de cosméticos terapêuticos - uma combinação da minha prática como terapeuta com a paixão por criar produtos, como shampoos sólidos, sabonetes, cremes para dor e velas terapêuticas - sabia que o desafio maior seria o investimento inicial. Foi nessa busca que me deparei com a opção de crédito com garantia de imóvel e encontrei a Libra Crédito. O processo foi fácil e o atendimento foi ótimo, consegui o suporte financeiro necessário para dar o primeiro passo com o projeto."
   }
 ];
 
